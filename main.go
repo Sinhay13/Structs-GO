22dce74540fbd1444d8320e5463cece8e213ab2b
Yahnis allegret <yahnis.allegret@yahoo.com>
2023-10-09 09:43:00 +0900

    test git

--- main.go
+++ main.go
@@ -4,52 +4,51 @@ import "fmt"
 
 type person struct { // initiate like a dictionary in python
 	firstName string
-	lastName string 
-	//contact contactInfo // we add contact 
+	lastName  string
+	//contact contactInfo // we add contact
 	contactInfo
 }
 
 type contactInfo struct {
-	email string
-	zipCode int 
-
+	email   string
+	zipCode int
 }
 
-func main(){
+func main() {
 
 	//alex := person{"Alex", "Anderson"} // first way if we respect the order of person
-	//alex:= person{firstName:"Alex",lastName:"Anderson"}// second way 
+	//alex:= person{firstName:"Alex",lastName:"Anderson"}// second way
 
-	// var alex person // third way 
+	// var alex person // third way
 	// alex.firstName="Alex"
 	// alex.lastName="Anderson"
 
 	// fmt.Println(alex)
-	// fmt.Printf("%+v", alex) // %+v to show all fields 
+	// fmt.Printf("%+v", alex) // %+v to show all fields
 
-	jim:= person{
+	jim := person{
 		firstName: "Jim",
-		lastName: "Party",
+		lastName:  "Party",
 		contactInfo: contactInfo{
-			email: "jim@gmail.com",
+			email:   "jim@gmail.com",
 			zipCode: 94000,
 		},
 	}
 	// jim.updateName("Jimmy")
-	jimPointer := &jim // pointer style
-	jimPointer.updateName("Jimmy")// pointer style  
+	jimPointer := &jim             // pointer style
+	jimPointer.updateName("Jimmy") // pointer style
 	jim.print()
 }
 
-// func (p person) updateName (newFirstName string) { // to update Name 
+// func (p person) updateName (newFirstName string) { // to update Name
 // 	p.firstName=newFirstName
 // }
 
-//update Name function pointer style 
+// update Name function pointer style
 func (pointerToPerson *person) updateName(newFirstName string) {
 	(*pointerToPerson).firstName = newFirstName
 }
 
-func (p person) print() { // to print 
+func (p person) print() { // to print
 	fmt.Printf("%+v", p)
 }
